7b89982591ab8f08e8098c9ff8d1a7dd0b1fdefa
cautiouspotato <afshaanlondhe4@gmail.com>
2020-01-01 22:37:00 +0530

    something isn right

--- 12n_body.c
+++ 12n_body.c
@@ -4,12 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 // stolen from: https://stackoverflow.com/a/8287124
-int *intdup(int const *src, int *dest, size_t len)
+int *intdup(int const *src, size_t len)
 {
-    dest = malloc(len * sizeof(int));
-    memcpy(dest, src, len * sizeof(int));
+    int *p = malloc(len * sizeof(int));
+    memcpy(p, src, len * sizeof(int));
+    return p;
+}
+
+int abssum(int *array)
+{
+    int sum;
+    for (size_t i = 0; i < sizeof array / sizeof array[0] + 1; i++) {
+        sum += abs(*(array + i));
+    }
+    return sum;
 }
 
 int calcgrav(int bdy1pos[], int bdy2pos[], int bdy1vel[], int bdy2vel[], size_t arrsize)
@@ -24,61 +33,57 @@ int calcgrav(int bdy1pos[], int bdy2pos[], int bdy1vel[], int bdy2vel[], size_t 
             bdy2vel[i] -= 1;
         }
     }
-    printf("vel1 %i\n", bdy1vel[0]);
 }
 
-int *calcpos(int vel[], int pos[], size_t arrsize)
+int calcpos(int vel[], int pos[], size_t arrsize)
 {
     for (size_t i = 0; i < arrsize; i++) {
         pos[i] += *(vel + i);
     }
-    printf("m2 %i\n", pos[0]);
-
-    return pos;
 }
 
 int main(int argc, char const *argv[])
 {
     int arrsize = 3;
-    int *nm1;
+    int mooncount = 4;
+    int moonenergy;
 
-    // positions of Io, Europa, Ganymede, and Callisto (x y z)
-    int muns[4][3] = {{14,  2,  8},
-                      { 7,  4, 10},
-                      { 1, 17, 16},
-                      {-4, -1,  1}};
-    // inti velocities of Io, Europa, Ganymede, and Callisto (x y z)
+    // init positions of Io, Europa, Ganymede, and Callisto (x y z)
+    int moons[4][3] = {{14, 2, 8},
+                       {7, 4, 10},
+                       {1, 17, 16},
+                       {-4, -1, 1}};
+    // init velocities of Io, Europa, Ganymede, and Callisto (x y z)
     int vels[4][3] = {{0, 0, 0},
                       {0, 0, 0},
                       {0, 0, 0},
                       {0, 0, 0}};
 
-    // apply gravity
-    for (size_t i = 0; i < arrsize + 1; i++) {
-        for (size_t j = 0; j < arrsize + 1; j++) {
-            if (muns[i] != muns[j]) {
-                printf("set %i %i \n", i, j);
-                calcgrav(muns[i], muns[j], vels[i], vels[j], arrsize);
+    // run the sim for a thousand steps
+    for (size_t i = 0; i < 1000; i++) {
+        // go over all pairs of moons and apply gravity
+        for (size_t i = 0; i < mooncount - 1; i++) {
+            for (size_t j = i + 1; j < mooncount; j++) {
+                if (i != j) {
+                    printf("cur pair %i %i \n", i, j);
+                    calcgrav(moons[i], moons[j], vels[i], vels[j], arrsize);
+                }
             }
+        }
+
+        // apply velocity, update moon positions
+        for (size_t i = 0; i < mooncount; i++) {
+            int *vel;
+            vel = intdup(vels[i], arrsize);
+            calcpos(vel, moons[i], arrsize);
+            printf("pos %i\n", moons[i][0]);
+            printf("velocity %i\n", vels[i][0]);
         }
     }
 
-    // calcgrav(m1, m2, v1, v2, sizeof m2 / sizeof m2[0]);
-    // printf("velocity1 %i\n", v1[0]);
-
-    int *vel;
-    // intdup(v1, vel, sizeof v1 / sizeof v1[0]);
-    // nm1 = calcpos(m1, vel, sizeof m1 / sizeof m1[0]);
-
-    // printf("velocity2 %i\n", v1[0]);
-
-    for (size_t i = 0; i < 3; i++) {
-        // printf("%i ", *(nm1 + i));
+    for (size_t i = 0; i < mooncount; i++) {
+        moonenergy += abssum(vels[i]) * abssum(moons[i]);
     }
-    printf("\n");
 
-    for (size_t i = 0; i < 3; i++) {
-        // printf("%i ", v1[i]);
-    }
-    printf("\n");
+    printf("total energy of the system: %i\n", moonenergy);
 }
