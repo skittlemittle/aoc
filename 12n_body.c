548a1cbdb279b61df2008367f73dba148892a7fd
cautiouspotato <afshaanlondhe4@gmail.com>
2020-01-01 13:11:07 +0530

    fInIshInG In NeXt DeCaDE

--- 12n_body.c
+++ 12n_body.c
@@ -4,64 +4,81 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 // stolen from: https://stackoverflow.com/a/8287124
-int *intdup(int const *src, size_t len)
+int *intdup(int const *src, int *dest, size_t len)
 {
-    int *p = malloc(len * sizeof(int));
-    memcpy(p, src, len * sizeof(int));
-    return p;
+    dest = malloc(len * sizeof(int));
+    memcpy(dest, src, len * sizeof(int));
 }
 
-
-int calcgrav(int moon1pos[], int moon2pos[], int moon1vel[], int moon2vel[], size_t sizepos)
+int calcgrav(int bdy1pos[], int bdy2pos[], int bdy1vel[], int bdy2vel[], size_t arrsize)
 {
-    for (size_t i = 0; i < sizepos; i++) {
-
-        if (moon1pos[i] > moon2pos[i]) {
-            moon1vel[i] -= 1;
-            moon2vel[i] += 1;
+    for (size_t i = 0; i < arrsize; i++) {
+        if (bdy1pos[i] > bdy2pos[i]) {
+            bdy1vel[i] -= 1;
+            bdy2vel[i] += 1;
         }
-        else if (moon1pos[i] < moon2pos[i]) {
-            moon1vel[i] += 1;
-            moon2vel[i] -= 1;
-        }  
+        else if (bdy1pos[i] < bdy2pos[i]) {
+            bdy1vel[i] += 1;
+            bdy2vel[i] -= 1;
+        }
     }
-    printf("vel1 %i\n", moon1vel[0]);
+    printf("vel1 %i\n", bdy1vel[0]);
 }
 
-
-int *calcpos(int moonvel[], int moonpos[], size_t sizepos)
+int *calcpos(int vel[], int pos[], size_t arrsize)
 {
-
-    for (size_t i = 0; i < sizepos; i++) {
-        moonpos[i] += *(moonvel + i);
+    for (size_t i = 0; i < arrsize; i++) {
+        pos[i] += *(vel + i);
     }
-    printf("m2 %i\n", moonpos[0]);
+    printf("m2 %i\n", pos[0]);
 
-    return moonpos;
+    return pos;
 }
-
 
 int main(int argc, char const *argv[])
 {
-    int m1[3] = {2, 11, 22};
-    int m2[3] = {3, 4, 5};
-    int v1[3] = {0, 0, 0};
-    int v2[3] = {0, 0, 0};
+    int arrsize = 3;
     int *nm1;
 
-    calcgrav(m1, m2, v1, v2, sizeof m2 / sizeof m2[0]);
-    printf("velocity1 %i\n", v1[0]);
+    // positions of Io, Europa, Ganymede, and Callisto (x y z)
+    int muns[4][3] = {{14,  2,  8},
+                      { 7,  4, 10},
+                      { 1, 17, 16},
+                      {-4, -1,  1}};
+    // inti velocities of Io, Europa, Ganymede, and Callisto (x y z)
+    int vels[4][3] = {{0, 0, 0},
+                      {0, 0, 0},
+                      {0, 0, 0},
+                      {0, 0, 0}};
 
-    int *vel = intdup(v1, sizeof v1 / sizeof v1[0]);
-    nm1 = calcpos(m1, vel, sizeof m1 / sizeof m1[0]);
-
-    printf("velocity2 %i\n", v1[0]);
-
-    for (size_t i = 0; i < 3; i++) { 
-        printf("%i\n", *(nm1 + i));
-        printf("fv %i\n", v1[i]);
+    // apply gravity
+    for (size_t i = 0; i < arrsize + 1; i++) {
+        for (size_t j = 0; j < arrsize + 1; j++) {
+            if (muns[i] != muns[j]) {
+                printf("set %i %i \n", i, j);
+                calcgrav(muns[i], muns[j], vels[i], vels[j], arrsize);
+            }
+        }
     }
 
-    return 0;
+    // calcgrav(m1, m2, v1, v2, sizeof m2 / sizeof m2[0]);
+    // printf("velocity1 %i\n", v1[0]);
+
+    int *vel;
+    // intdup(v1, vel, sizeof v1 / sizeof v1[0]);
+    // nm1 = calcpos(m1, vel, sizeof m1 / sizeof m1[0]);
+
+    // printf("velocity2 %i\n", v1[0]);
+
+    for (size_t i = 0; i < 3; i++) {
+        // printf("%i ", *(nm1 + i));
+    }
+    printf("\n");
+
+    for (size_t i = 0; i < 3; i++) {
+        // printf("%i ", v1[i]);
+    }
+    printf("\n");
 }
