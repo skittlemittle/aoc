3ddd4e3bc004cff1e7ed75a3b7336ffe8bf85f5f
skitt <cautiouspotato@gmail.com>
2020-12-11 20:53:33 +0530

    11,2

--- 2020/11.js
+++ 2020/11.js
@@ -5,7 +5,7 @@ let WIDTH, HEIGHT;
 const states = { floor: ".", empty: "L", full: "#" };
 
 // all eight adjacent neighbors
-function getNeighbors(grid, x, y) {
+function getNeighborsV1(grid, x, y) {
   const neighbors = [];
   for (let i = -1; i < 2; i++) {
     for (let j = -1; j < 2; j++) {
@@ -22,6 +22,38 @@ function getNeighbors(grid, x, y) {
   return neighbors;
 }
 
+function raycast(grid, x, y, xstep, ystep) {
+  let hit = false;
+  while(!hit) {
+    x += xstep;
+    y += ystep;
+    if (y < 0 || y >= WIDTH || x < 0 || x >= HEIGHT) break;
+
+    const tile = grid[x][y];
+    if (tile === states.full) {
+      hit = states.full;
+    } else if (tile == states.empty) {
+      hit = states.empty;
+    }
+  }
+  return hit;
+}
+
+function getNeighbors(grid, x, y) {
+  const neighbors = [];
+  neighbors.push(raycast(grid, x, y, -1, 0)); //up
+  neighbors.push(raycast(grid, x, y, 1, 0)); //down
+  neighbors.push(raycast(grid, x, y, 0, 1)); //right?
+  neighbors.push(raycast(grid, x, y, 0, -1)); //left?
+  //slanty bois
+  neighbors.push(raycast(grid, x, y, -1, -1));
+  neighbors.push(raycast(grid, x, y, -1, 1));
+  neighbors.push(raycast(grid, x, y, 1, -1));
+  neighbors.push(raycast(grid, x, y, 1, 1));
+
+  return neighbors.filter(it => it !== false);
+}
+
 function one(input) {
   // copying what a noob
   const next = [...Array(HEIGHT)].map(() => (new Array(WIDTH)));
@@ -35,7 +67,7 @@ function one(input) {
       if (state === states.empty && !neighbors.includes(states.full)) {
         next[x][y] = states.full;
         changed = true;
-      } else if (state === states.full && neighbors.filter(n => n === states.full).length >= 4) {
+      } else if (state === states.full && neighbors.filter(n => n === states.full).length >= 5) {
         next[x][y] = states.empty;
         changed = true;
       } else {
@@ -51,6 +83,8 @@ fs.readFile("inputs/11.txt", "utf8", (_, data) => {
   input.splice(-1, 1);
   WIDTH = input[0].length;
   HEIGHT = input.length;
+
+  console.log(input);
 
   let curr = [...input];
   while (true) {
