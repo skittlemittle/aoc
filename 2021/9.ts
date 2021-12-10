1014b650eff0476f21aec6f94962fca6fb13f091
skittlemittle <cautiouspotato@gmail.com>
2021-12-10 10:28:59 +0530

    9

--- 2021/9.ts
+++ 2021/9.ts
@@ -24,11 +24,56 @@ function one(input: number[][]) {
   return dangerRating.reduce((p, c) => p + c);
 }
 
-// flood fill from the minimas
-function two() {}
+type point = { x: number; y: number };
+const height = (p: point) => input[p.y][p.x];
+const mark = (p: point) => (input[p.y][p.x] = -1);
+
+const neighbors: point[] = [
+  { x: 1, y: 0 },
+  { x: -1, y: 0 },
+  { x: 0, y: 1 },
+  { x: 0, y: -1 },
+];
+
+function floodBasin(point: point, basin: point[]) {
+  basin.push(point);
+  mark(point);
+  const n = neighbors
+    .map((neighbor) => {
+      const x = point.x + neighbor.x;
+      const y = point.y + neighbor.y;
+      if (x >= 0 && x < input[0].length && y >= 0 && y < input.length) {
+        return { x, y };
+      }
+      return undefined;
+    })
+    .filter((p) => p !== undefined);
+
+  n.forEach((p) => {
+    if (height(p) < 9 && height(p) >= 0) floodBasin(p, basin);
+  });
+  return basin;
+}
+
+// flood fill
+function two(minimas: point[]) {
+  const sizes = minimas.map((point) => floodBasin(point, []).length);
+  return sizes
+    .sort((a, b) => a - b)
+    .splice(sizes.length - 3)
+    .reduce((p, c) => p * c);
+}
 
 file.readFile("./inputs/9.txt", "utf8", (_, data) => {
   input = data.split("\n").map((l) => l.split("").map((it) => parseInt(it)));
   input.pop();
   console.log(one(input));
+
+  const minimas: point[] = [];
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[0].length; x++) {
+      if (compare(x, y)) minimas.push({ x, y });
+    }
+  }
+  console.log(two(minimas));
 });
